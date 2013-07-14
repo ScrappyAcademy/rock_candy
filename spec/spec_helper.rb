4b6e656c79c59fdb66f08531046ef9c8b08d8113
Aaron Kromer <aaron.kromer@gmail.com>
2013-07-13 20:00:33 -0400

    Add RSpec setup defaults.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,13 @@ if ENV['COVERALLS']
   require 'coveralls'
   Coveralls.wear!
 end
+
+Dir[File.expand_path('../support/**/*.rb', __FILE__)].each{|f| require f}
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+
+  config.order = 'random'
+end
